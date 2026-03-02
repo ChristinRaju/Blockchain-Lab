711e4abfdb419913a7fa5ac5bf27fc2cb6932771
ChristinRaju <2006christinraju@gmail.com>
2026-03-02 10:29:53 +0530

    Update EvenOdd.sol

--- EvenOdd.sol
+++ EvenOdd.sol
@@ -14,3 +14,19 @@ contract EvenOdd {
         }
     }
 }
+
+
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract EvenOdd {
+
+    function checkEvenOdd(uint _number) public pure returns (string memory) {
+        if (_number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
